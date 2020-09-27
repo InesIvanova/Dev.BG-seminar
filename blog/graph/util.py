8cf4bb846d63556f44e63c7b2df609e0af8b22b6
Ines Ivanova <ines.iv.ivanova@gmail.com>
2020-09-27 17:03:11 +0300

    Create validation decorator for decorators
    Create decorator which can take validation functions
    Decorate the mutate method with the new decorator and add validation
    functions to it
    Change the validate_email so that it can meet the new functionality
    needs

--- blog/graph/util.py
+++ blog/graph/util.py
@@ -3,8 +3,7 @@ import graphene
 from blog.blog_post import db
 from blog.graph.input import IUser
 from blog.graph.objects import User
-from blog.utils.exceptions import InvalidInput
-from blog.utils.validation_helpers import validate_email
+from blog.utils.validation_helpers import validate_email, validate
 from blog.utils.db_helpers import save_user, save_email
 
 
@@ -14,8 +13,8 @@ class CreateUser(graphene.Mutation):
 
     person = graphene.Field(User)
 
-    @validate_email
-    def mutate(root, info, user_data=None):
+    @validate(validate_email,)
+    def mutate(self, root, info, user_data=None):
         user = User(first_name=user_data.first_name, last_name=user_data.last_name)
         user_pk = save_user(user)
         save_email(user_data.email, user_pk)
--- blog/utils/validation_helpers.py
+++ blog/utils/validation_helpers.py
@@ -4,11 +4,20 @@ from blog.constants import EMAIL_REGEX
 from blog.utils.exceptions import InvalidInput
 
 
-def validate_email(function):
-    def wrapper(*args, **kwargs):
-        email_data = kwargs.get('user_data').get('email')
-        email = email_data.get('email')
-        if email and not re.search(EMAIL_REGEX, email):
-            raise InvalidInput(f"Invalid email {email}")
-        function(*args, **kwargs)
-    return wrapper
+def validate_email(**kwargs):
+    email_data = kwargs.get('user_data').get('email')
+    email = email_data.get('email')
+    if email and not re.search(EMAIL_REGEX, email):
+        raise InvalidInput(f"Invalid email {email}")
+
+
+def validate(*validation_functions):
+    def decorator(decorated_function):
+        def wrapper(*args, **kwargs):
+            for validation_func in validation_functions:
+                if not callable(validation_func):
+                    raise TypeError("The object in the decorator should be a function reference")
+                validation_func(**kwargs)
+            decorated_function(*args, **kwargs)
+        return wrapper
+    return decorator
